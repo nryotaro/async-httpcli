100f975e3c50a7197656405a40ae9f5fbbef6e32
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-05-11 19:08:44 +0900

    ssl works

--- src/test/kotlin/org/nryotaro/nettysample/server/Server.kt
+++ src/test/kotlin/org/nryotaro/nettysample/server/Server.kt
@@ -7,13 +7,26 @@ import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.handler.codec.http.*
+import io.netty.handler.ssl.SslContext
 import io.netty.handler.ssl.SslHandler
+import io.netty.handler.ssl.util.SelfSignedCertificate
+import io.netty.handler.stream.ChunkedNioFile
 import io.netty.handler.stream.ChunkedWriteHandler
 import org.apache.http.HttpStatus
 import java.net.InetSocketAddress
 import java.io.RandomAccessFile
+import javax.net.ssl.SSLEngine
 
 
+
+
+/**
+ *  test timeout ssl
+ *  connection timeout
+ *  file contents consistency
+ *
+ *  ssl
+ */
 
 class Server {
 
@@ -21,10 +34,20 @@ class Server {
 
     init {
         val group: EventLoopGroup = NioEventLoopGroup()
+
+        val cert: SelfSignedCertificate  = SelfSignedCertificate()
+
+        val context: SslContext = SslContext.newServerContext(cert.certificate(), cert.privateKey())
+
+
+
         bootstrap.group(group).channel(NioServerSocketChannel::class.java).localAddress(InetSocketAddress(8080))
                 .childHandler(object: ChannelInitializer<SocketChannel>(){
                     override fun initChannel(channel: SocketChannel) {
                         val pipeline = channel.pipeline()
+                        if (context != null) {
+                            pipeline.addLast("ssl", context.newHandler(channel.alloc()));
+                        }
                         pipeline.addLast("decoder", HttpRequestDecoder())
                         pipeline.addLast("encoder", HttpResponseEncoder())
                         //pipeline.addLast("compressor", HttpContentCompressor())
@@ -39,22 +62,41 @@ class Server {
     }
 }
 
+// Simple extends Channel inboudn handler
 class SimpleHandler: SimpleChannelInboundHandler<HttpObject>() {
     override fun channelRead0(ctx: ChannelHandlerContext, msg: HttpObject) {
 
-        val file = RandomAccessFile("/Users/nryotaro/foobar.txt", "r")
-        val resp = DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
-        resp.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8")
+        if(msg is DefaultHttpRequest) {
 
-        // if keep alive
-        resp.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length())
-        resp.headers().set( HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE)
-        // end
+        }
+        if(msg is LastHttpContent) {
+            val file = RandomAccessFile("/Users/nryotaro/foobar.txt", "r")
+            val resp = DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+            resp.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8")
 
-        ctx.write(resp)
-        ctx.write(DefaultFileRegion(file.channel, 0, file.length()))
-        val future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT)
+            // if keep alive
+            resp.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length())
+            resp.headers().set( HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE)
+            // end
+
+            ctx.write(resp)
+            if (ctx.pipeline().get(SslHandler::class.java) == null) {
+                ctx.write(DefaultFileRegion(
+                        file.channel, 0, file.length()))
+            } else {
+                ctx.write(ChunkedNioFile(file.channel))
+            }
+            val future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT)
+        }
+
     }
+    override fun exceptionCaught(ctx: ChannelHandlerContext , cause: Throwable ){
+        println("errror")
+    }
+}
+
+class A: ChannelInboundHandlerAdapter() {
+
 }
 fun main(args : Array<String>) {
 
